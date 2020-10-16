8cd0d9248100dc0e5df537f709cefc601d73185f
thiccsupreme <57068146+thiccsupreme@users.noreply.github.com>
2020-10-16 11:45:57 -0400

    Made the bot's output an embed!

--- src/info/quote.js
+++ src/info/quote.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 module.exports = {
     name: "quote",
     run: async (client, message, args) => {
@@ -5,6 +7,7 @@ module.exports = {
         const channelID = args[0]
         const msgID = args[1]
 
+        const clientAV = client.user.displayAvatarURL
 
         if (!channelID || !msgID) {
             return message.channel.send("Please use the format `.quote <channel id> <message id>`")
@@ -18,7 +21,17 @@ module.exports = {
 
             const msgContent = msg.content
 
-            message.channel.send(msgContent)
+            const author = msg.author.tag
+
+
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`Quote a User`)
+                .setColor(0x5126c7)
+                .setDescription(`Channel ID: __${channelID}__\nMessage ID: __${msgID}__\n Author: __${author}__`)
+                .addField("Message Content:", `**${msgContent}**`, false)
+                .setFooter(client.user.tag, client.user.displayAvatarURL)
+            message.channel.send(embed)
+
         } catch (err) {
             if (err) {
                 return message.channel.send("Please use the format `.quote <channel id> <message id>`")
