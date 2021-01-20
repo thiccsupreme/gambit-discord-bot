25a01b48d34cb0a75764a43dc0b1072b5fd1e527
thiccsupreme <57068146+thiccsupreme@users.noreply.github.com>
2021-01-20 10:42:13 -0500

    Reworked the ban command!
    To adjust who can ban go to line 8 and adjust permissions!

--- src/mod/ban.js
+++ src/mod/ban.js
@@ -2,35 +2,30 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "ban",
-    category: "info",
-    description: "bans the user from the guild",
-    run: (client, message, args) => {
-        if (!message.member.roles.find(r => r.name === "KICK_MEMBERS")) return message.channel.send("You do not have permission to send this command")
+    run: (client, message) => {
+        let member = message.mentions.users.first();
 
-            const user = message.mentions.users.first();
-
-            if (user) {
-                const member = message.guild.member(user);
-
-                if (member) {
-                    member.ban().then(() => {
-                        const Embed = new Discord.MessageEmbed()
-                        .setColor(0x5126c7)
-                        .setDescription(`Successfully banned ${member}`);
-                        message.channel.send(Embed);
-                    }).catch(err => {
-                        const Embed1 = new Discord.MessageEmbed()
-                          .setColor(0x5126c7)
-                          .setDescription(`I was unable to ban the member`);
-                          message.channel.send(Embed1);
-                        console.log(err);
-                    });
-                } else {
-                    message.reply("That user is not in this server")
-                }
+        if (message.member.hasPermission("ADMINISTRATOR")) {
+            if (member) {
+                const memberTarget = message.guild.members.cache.get(member.id);
+                memberTarget.ban();
+                const embed = new Discord.MessageEmbed()
+                    .setTitle("Successfully Banned the Mentioned User!")
+                    .setColor("GREEN")
+                    .setThumbnail(member.displayAvatarURL())
+                    .addField("Username", memberTarget, true)
+                    .addField("ID", member.id, true)
+                    .setTimestamp()
+                message.channel.send(embed)
             } else {
-                message.reply('Please specify a user');
-
+                const embed1 = new Discord.MessageEmbed()
+                    .setTitle("Error")
+                    .setColor("RED")
+                    .setDescription("Error: Could not ban the mentioned member! Please make sure you have correctly formatted the command!\n\n`.ban <user>`")
+                message.channel.send(embed1)
             }
+        } else {
+            message.channel.send("You do not have the proper permissions to use this command.")
+        }
     }
 }
