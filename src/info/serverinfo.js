0c63bbc2a7ec9c66ca0dbbfad0060b0ddc8b183f
thiccsupreme <57068146+thiccsupreme@users.noreply.github.com>
2021-01-22 15:09:46 -0500

    Adjusted Server Owner Field ✍️

--- src/info/serverinfo.js
+++ src/info/serverinfo.js
@@ -7,7 +7,7 @@ module.exports = {
             .setTitle(`**${message.guild.name}'s Info**`)
             .setThumbnail(message.guild.iconURL())
             .setColor(0x5126c7)
-            .addField("Server Owner", message.guild.owner, false)
+            .addField("Server Owner", `<@${message.guild.ownerID}>`, false)
             .addField("Server Creation Date", new Date(message.guild.createdAt).toLocaleDateString(), false)
             .addField("Member Count", message.guild.memberCount, true)
             .addField("MFA Level", message.guild.mfaLevel, true)
