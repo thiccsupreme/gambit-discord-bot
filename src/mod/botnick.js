05bfa8c621993aa5d094e818a6d65a377a25a297
thiccsupreme <57068146+thiccsupreme@users.noreply.github.com>
2021-01-23 01:46:00 -0500

    Delete src/mod directory

--- src/mod/botnick.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "setbotnick", 
-    run: (client, message, args) => {
-        if (!message.guild.me.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("You do not have permission to use this command");
-        var nickname = message.content.split (" ").slice (1).join (" ");
-        message.guild.members.get(client.user.id).setNickname(nickname)
-    }
-}
